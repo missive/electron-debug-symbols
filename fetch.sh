ecb99d72d1699d4665d8d42b9960dfac91b8a980
Etienne Lemay <etienne@heliom.ca>
2019-10-08 12:51:41 -0400

    Improve fetch and JS helper - Only fetched given platform - Download dsym.zip instead of symbols.zip (they changed that some time ago)

--- fetch.sh
+++ fetch.sh
@@ -5,20 +5,19 @@ if [ -z "$ELECTRON_VERSIONS" ]; then
   exit 1
 fi
 
+if [ -z "$PLATFORM" ]; then
+  echo "Need to set PLATFORM (win32-ia32 || win32-x64 || darwin-x64 || linux-x64)"
+  exit 1
+fi
+
 for VERSION in `echo $ELECTRON_VERSIONS`; do
-  mkdir -p symbols/{win32-ia32-$VERSION,win32-x64-$VERSION,darwin-$VERSION,linux-$VERSION}
+  mkdir -p symbols/$VERSION/$PLATFORM
 
   if [ `which bsdtar` ] && [ ! $FORCE_UNZIP ]; then
     cmd="bsdtar -xvf-"
-    ( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip | $cmd )
-    ( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-x64-symbols.zip | $cmd )
-    ( cd symbols/darwin-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip | $cmd )
-    ( cd symbols/linux-$VERSION; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip | $cmd )
+    ( cd symbols/$VERSION/$PLATFORM ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-${PLATFORM}-dsym.zip | $cmd )
   elif [ `which unzip` ]; then
     cmd="unzip -x"
-    ( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip > /tmp/win32-ia32.zip ; $cmd /tmp/win32-ia32.zip )
-    ( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-x64-symbols.zip > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip )
-    ( cd symbols/darwin-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
-    ( cd symbols/linux-$VERSION ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip > /tmp/linux.zip ; $cmd /tmp/linux.zip )
+    ( cd symbols/$VERSION/$PLATFORM ; wget -O - https://github.com/electron/electron/releases/download/v${VERSION}/electron-v${VERSION}-${PLATFORM}-dsym.zip > /tmp/${PLATFORM}.zip ; $cmd /tmp/${PLATFORM}.zip )
   fi
 done
--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 const path = require('path')
-const fs = require('fs')
-
-const platforms = [
-  'darwin',
-  'linux',
-  'win32-ia32',
-  'win32-x64',
-]
 
 module.exports = {
-  // return all symbol directories (including all versions)
-  paths: ({ relative } = {}) => {
+  path: ({ version, platform, relative } = {}) => {
     let root = relative ? './' : __dirname
-    let dirs = fs.readdirSync(path.join(root, 'symbols'))
-    return dirs.map((dir) => {
-      return path.join(root, 'symbols', dir, 'electron.breakpad.syms')
-    })
-  },
-
-  // return all symbol directories for a specific electron version
-  pathsForVersion: (version, { relative } = {}) => {
-    return platforms.map((platform) => {
-      let root = relative ? './' : __dirname
-      return path.join(__dirname, 'symbols', platform + '-' + version, 'electron.breakpad.syms')
-    })
+    return path.join(root, 'symbols', version, platform, 'breakpad_symbols')
   }
 }
